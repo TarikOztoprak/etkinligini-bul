370811d0fbb9b92595211782dc09280f098a48b0
Muhammet Tarık ÖZTOPRAK <tarik.oztoprak@hotmail.com>
2022-06-19 21:37:52 +0300

    first commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Assets/Styles/NavBar.css'
+function NavBar() {
+  return (
+    <div className='Navigation'>
+        <h1 className='Header'>Etkinligini Bul</h1>
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/Components/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Assets/Styles/Slider.css'
+function Slider() {
+  return (
+    <div className='Slider'>
+     
+    </div>
+  )
+}
+
+export default Slider
